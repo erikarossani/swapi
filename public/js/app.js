3972096616dd1fd7224327b6e9a0f67c2709ca3d
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-11-11 08:07:08 -0500

    poniendo estilos y select

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,58 @@
-var template = '<div class="col s12 m4">' +
-		    '<div class="card horizontal hoverable">' +
-		      	'<div class="card-stacked">' +
-		        	'<div class="card-content brown white-text">' +
-		          		'<p>Hi, my name is <strong>{{name}}</strong></p>' +
-		        	'</div>' +
-			        '<div class="card-action">' +
-			          	'<a data-show-url="{{url}}" class= "about">See more about me</a>' +
-			        '</div>' +
-			    '</div>' +
-	    	'</div>' +
-	  	'</div>';
-
+var template = '<div class="col s12 m6">' +
+                '<div class="card horizontal hoverable">' +
+                    '<div class="card-stacked">' +
+                        '<div class="card-content indigo darken-4 white-text">' +
+                            '<p>Hi, my name is <strong>{{name}}</strong></p>' +
+                        '</div>' +
+                        '<div class="card-action">' +
+                            '<a data-show-url="{{url}}" class="about">Detalle personaje</a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>';   
 
 $(document).ready(function(){
-
-	var formatResponse= function(response){
-		$("#total").text(response.results.length);//results.length ó count
-		var personajes = "";
-		$.each(response.results, function(i, personaje){
-			personajes += template
-			           .replace("{{name}}", personaje.name)
-			           .replace("{{url}}", personaje.url);
-		});
-
-		$("#people").html(personajes);
-		$("#next").attr("data-url", response.next);
-		$("#previous").attr("data-url", response.previous);
-		if(!response.next){
-			$("#next").fadeOut();
-		}else{
-			$("#next").fadeIn();
-		}
-
-		if(!response.previous){
-			$("#previous").fadeOut();
-		}else{
-			$("#previous").fadeIn();
-		}
-
-
-	};
-
- $.getJSON("http://swapi.co/api/people/", formatResponse);
     
-	$("#next").click(function(event){
-		event.preventDefault();
-		var url = $(this).attr("data-url");
-		 $.getJSON(url, formatResponse);
-		
-		});
+    var formatResponse = function(response){          
+        $("#total").text(response.results.length);
+        var personajes  = "";
+        $.each(response.results, function(i, personaje){
+            personajes += template
+                          .replace("{{name}}", personaje.name)
+                          .replace("{{url}}", personaje.url);
+        });
+        $("#people").html(personajes);
+        $("#next").attr("data-url", response.next);
+        $("#previous").attr("data-url", response.previous);
+        if(!response.next){
+            $("#next").fadeOut();
+        } else {
+            $("#next").fadeIn()
+        }
 
-	$("#previous").click(function(event){
-		event.preventDefault();
-		var url = $(this).attr("data-url");
-		 $.getJSON(url, formatResponse);
-		
-		});
+        if(!response.previous){
+            $("#previous").fadeOut();
+        } else {
+            $("#previous").fadeIn();
+        }
+    };
 
-	$("#people").on("click",".about", function(event){//con ajax siempre se debe meter el evento on
-		event.preventDefault();
-		alert("hola");
-	});
+    $.getJSON("http://swapi.co/api/people/", formatResponse);
 
+    $("#next").click(function(event){
+        event.preventDefault();
+        var url = $(this).attr("data-url");  
+        $.getJSON(url, formatResponse);  
+    });
+
+    $("#previous").click(function(event){
+        event.preventDefault();
+        var url = $(this).attr("data-url");  
+        $.getJSON(url, formatResponse);  
+    });
+
+    $("#people").on("click",".about",function(event){
+        event.preventDefault();
+        alert("hola");
+    });
 });
---
+++ public/js/especies.js
@@ -0,0 +1,64 @@
+var categoria = '<option value="{{num}}">{{especie}}</option>';
+
+var template = '<div class="col s12 m6">' +
+                '<div class="card horizontal hoverable">' +
+                    '<div class="card-stacked">' +
+                        '<div class="card-content indigo darken-4 white-text">' +
+                            '<p>Hi, my name is <strong>{{name}}</strong></p>' +
+                        '</div>' +
+                        '<div class="card-action">' +
+                            '<a data-show-url="{{url}}" class="about">Detalle personaje</a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'; 
+
+$(document).ready(function(){
+
+    var formatResponse = function(response){          
+        var personajes  = "";
+        $.each(response.results, function(i, personaje){
+            personajes += template
+                          .replace("{{name}}", personaje.name)
+                          .replace("{{url}}", personaje.url);
+        });
+        $("#people").html(personajes);
+    };
+
+    var nombreEspecies = function(response){        
+        var especies  = "";
+        $.each(response.results, function(i, especie){
+            var posicion = "";
+            var urlEspecies = "http://swapi.co/api/people/";
+
+            $.each(especie.people, function (i, valor) {
+			posicion += valor.replace( urlEspecies, "");
+		    });
+
+            for(var i = 0, l = especie.people.length; i < l; i++){
+                posicion += especie.people[i].substr(-3);
+            }
+            
+            console.log(posicion);
+            especies += categoria
+                        .replace("{{num}}", posicion)
+                        .replace("{{especie}}",especie.name);
+
+            var mostrarPersonajes = function(response){
+
+            	var array = $(this).val().split("/");
+                for(var i = 0, l = response.length; i < l; i++){
+                    $.getJSON(urlEspecies + posicion, formatResponse);
+                    var nameSpecie = template.replace("{{name}}", response.name);
+                }
+             }             
+
+             $("#contenedor").on("change", ("#especie"), mostrarPersonajes);
+        });
+
+        $("#especie").append(especies);
+    }
+    
+    $.getJSON("http://swapi.co/api/species/", nombreEspecies);
+
+});
